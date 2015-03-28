2fd87c5743465139a08e99539010e754e1382300
Josh Rincon <memonkey@gmail.com>
2015-03-28 14:36:23 -0700

    Added magnific popup and form support

--- vagrant/poetryandalcohol/static/application.js
+++ vagrant/poetryandalcohol/static/application.js
@@ -1,4 +1,23 @@
 $(function() {
+
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+          beforeOpen: function() {
+            if($(window).width() < 700) {
+              this.st.focus = false;
+            } else {
+              this.st.focus = '#name';
+            }
+          }
+        }
+      });
+
   var submit_form = function(e) {
     $.getJSON($SCRIPT_ROOT + '/get_author_poems', {
       author_id: $(this).attr('id')
